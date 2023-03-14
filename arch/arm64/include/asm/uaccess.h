432777848d59f7b90a49f5641a0726ccdda6d185
Mark Rutland <mark.rutland@arm.com>
2023-03-14 15:37:00 +0000

    arm64/include/asm/uaccess: Remove unnecessary earlyclobber
    Currently the asm constraints for __get_mem_asm() mark the value
    register as an earlyclobber operand. This means that the compiler can't
    reuse the same register for both the address and value, even when the
    value is not subsequently used.
    
    There's no need for the value register to be marked as earlyclobber, as
    it's only written to after the address register is consumed, even when
    the access faults.
    
    Remove the unnecessary earlyclobber.
    
    There should be no functional change as a result of this patch.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Robin Murphy <robin.murphy@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20230314153700.787701-5-mark.rutland@arm.com
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: hani <itshanibee@gmail.com>

--- arch/arm64/include/asm/uaccess.h
+++ arch/arm64/include/asm/uaccess.h
@@ -278,7 +278,7 @@ static inline void __user *__uaccess_mask_ptr(const void __user *ptr)
 	"	b	2b\n"						\
 	"	.previous\n"						\
 	_ASM_EXTABLE(1b, 3b)						\
-	: "+r" (err), "=&r" (x)						\
+	: "+r" (err), "=r" (x)						\
 	: "r" (addr), "i" (-EFAULT))
 
 #define __get_user_err(x, ptr, err)					\
