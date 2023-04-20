6d2df6b8951d2dc2a8dbbb90a31a0211530a0a6a
Tyler Nijmeh <tylernij@gmail.com>
2023-04-20 12:04:51 +0800

    kernel/sched/features: Optimize features
    * Do not give sleepers 50% more runtime
    * Enable NEXT_BUDDY for better cache locality
    * Do not reduce perceived CPU capacity while idle
    * Queue non-local tasks on a cache sharing CPU
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: hani <itshanibee@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -4,7 +4,7 @@
  * them to run sooner, but does not allow tons of sleepers to
  * rip the spread apart.
  */
-SCHED_FEAT(GENTLE_FAIR_SLEEPERS, true)
+SCHED_FEAT(GENTLE_FAIR_SLEEPERS, false)
 
 /*
  * Place new tasks ahead so that they do not starve already running
@@ -17,7 +17,7 @@ SCHED_FEAT(START_DEBIT, true)
  * wakeup-preemption), since its likely going to consume data we
  * touched, increases cache locality.
  */
-SCHED_FEAT(NEXT_BUDDY, false)
+SCHED_FEAT(NEXT_BUDDY, true)
 
 /*
  * Prefer to schedule the task that ran last (when we did
@@ -50,7 +50,7 @@ SCHED_FEAT(NONTASK_CAPACITY, false)
  * Queue remote wakeups on the target CPU and process them
  * using the scheduler IPI. Reduces rq->lock contention/bounces.
  */
-SCHED_FEAT(TTWU_QUEUE, false)
+SCHED_FEAT(TTWU_QUEUE, true)
 
 /*
  * When doing wakeups, attempt to limit superfluous scans of the LLC domain.
