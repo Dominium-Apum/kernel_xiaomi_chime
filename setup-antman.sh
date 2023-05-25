fda8b1baf4706c77c3339a57b766f772de75a2d0
hani <itshanibee@gmail.com>
2023-05-25 14:18:29 +0530

    antman: Moving to a personal fork of glibc patch

--- setup-antman.sh
+++ setup-antman.sh
@@ -21,7 +21,7 @@ bash build/autogen.sh
 make -j$(nproc)
 cd ..
 echo 'Patch for glibc'
-wget https://gist.githubusercontent.com/dakkshesh07/240736992abf0ea6f0ee1d8acb57a400/raw/a835c3cf8d99925ca33cec3b210ee962904c9478/patch-for-old-glibc.sh
+wget https://gist.githubusercontent.com/itsHanibee/fac63ea2fc0eca7b8d7dcbb7eb678c3b/raw/beacf8f0f71f4e8231eaa36c3e03d2bee9ae3758/patch-for-old-glibc.sh
 export PATH=$(pwd)/libarchive:$PATH
 bash patch-for-old-glibc.sh
 echo 'Done'
