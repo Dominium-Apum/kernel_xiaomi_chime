8c6fe078608cb34140fb29aa224b658e13289cdb
itsHanibee <itshanibee@gmail.com>
2023-06-16 17:07:04 +0530

    hani-ci: clean/add some stuff
    BOLT patch cleanup and using more of LLVM Tools
    
    Signed-off-by: itsHanibee <itshanibee@gmail.com>

--- hani-ci.sh
+++ hani-ci.sh
@@ -33,9 +33,6 @@ if [[ $1 = "-t" || $1 = "--tools" ]]; then
 	wget https://gist.githubusercontent.com/itsHanibee/fac63ea2fc0eca7b8d7dcbb7eb678c3b/raw/beacf8f0f71f4e8231eaa36c3e03d2bee9ae3758/patch-for-old-glibc.sh
 	export PATH=$(pwd)/libarchive:$PATH
 	bash patch-for-old-glibc.sh
-
-	echo 'Patch for BOLT on toolchain'
-	bash antman --patch=bolt
 fi
 
 # Regenerate defconfig file
@@ -48,6 +45,7 @@ fi
 if [[ $1 = "-b" || $1 = "--build" ]]; then
 	export ARCH=arm64
 	PATH=$PWD/toolchain/bin:$PATH
+	export USE_HOST_LEX=yes
 	mkdir -p out
 	make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 $DEFCONFIG
 	echo -e ""
@@ -59,7 +57,7 @@ if [[ $1 = "-b" || $1 = "--build" ]]; then
 	echo -e "*****************************"
 	echo -e ""
 	echo -e ""
-	make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 -j$(nproc) || exit 1
+	make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j$(nproc) || exit 1
 
 	kernel="out/arch/arm64/boot/Image"
 	dtbo="out/arch/arm64/boot/dtbo.img"
