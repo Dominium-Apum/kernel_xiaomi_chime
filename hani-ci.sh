8cdb82e456ccd0a7e097112db20df75f44f1a643
itsHanibee <itshanibee@gmail.com>
2023-12-27 00:17:12 +0530

    ci: leaner
    Signed-off-by: itsHanibee <itshanibee@gmail.com>

--- hani-ci.sh
+++ hani-ci.sh
@@ -21,18 +21,8 @@ if [[ $1 = "-t" || $1 = "--tools" ]]; then
 	echo 'Setting up toolchain in $(PWD)/toolchain'
 	bash antman -S || exit 1
 
-	echo 'Build libarchive for bsdtar'
-	git clone https://github.com/libarchive/libarchive || true
-	cd libarchive
-	bash build/autogen.sh
-	./configure
-	make -j$(nproc)
-	cd ..
-
 	echo 'Patch for glibc'
-	wget https://gist.githubusercontent.com/itsHanibee/fac63ea2fc0eca7b8d7dcbb7eb678c3b/raw/beacf8f0f71f4e8231eaa36c3e03d2bee9ae3758/patch-for-old-glibc.sh
-	export PATH=$(pwd)/libarchive:$PATH
-	bash patch-for-old-glibc.sh
+	bash antman --patch=glibc
 fi
 
 # Regenerate defconfig file
