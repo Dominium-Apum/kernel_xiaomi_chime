14e58d5726f3455cecb451be32b994e3f842e5da
hani <itshanibee@gmail.com>
2024-01-24 14:03:18 +0530

    drivers/kernelsu: Hardcode version
    Signed-off-by: hani <itshanibee@gmail.com>

--- drivers/kernelsu/ksu.h
+++ drivers/kernelsu/ksu.h
@@ -4,7 +4,8 @@
 #include "linux/types.h"
 #include "linux/workqueue.h"
 
-#define KERNEL_SU_VERSION KSU_VERSION
+#define KERNEL_SU_VERSION 11466
+// logic: (10000 + (git rev-list --count HEAD) for the driver source + 200)
 #define KERNEL_SU_OPTION 0xDEADBEEF
 
 #define CMD_GRANT_ROOT 0
