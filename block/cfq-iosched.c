3443aa0c2a2c9c6c01de1ffc4582b48548aaaf98
1582130940 <19632674+1582130940@users.noreply.github.com>
2024-02-12 02:57:07 +0800

    block/cfq-iosched: Optimize cfq-iosched
    * cfq_quantum = 64
    * cfq_back_penalty = 1
    * cfq_slice_idle = 0
    * cfq_group_idle = 0
    
    Co-authored-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Co-authored-by: str143 <alexfinhart@gmail.com>
    Co-authored-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: hani <itshanibee@gmail.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -23,7 +23,7 @@
  * tunables
  */
 /* max queue in one round of service */
-static const int cfq_quantum = 32;
+static const int cfq_quantum = 64;
 static const u64 cfq_fifo_expire[2] = { NSEC_PER_SEC / 4, NSEC_PER_SEC / 8 };
 /* maximum backwards seek, in KiB */
 static const int cfq_back_max = 32 * 1024;
@@ -33,7 +33,7 @@ static const u64 cfq_slice_sync = NSEC_PER_SEC / 10;
 static u64 cfq_slice_async = NSEC_PER_SEC / 25;
 static const int cfq_slice_async_rq = 2;
 static u64 cfq_slice_idle = 0;
-static u64 cfq_group_idle = 1;
+static u64 cfq_group_idle = 0;
 /* IOPP-cfq_rt_idle_only-v1.0.k4.19 */
 static int cfq_rt_idle_only = 0;
 static const u64 cfq_target_latency = (u64)NSEC_PER_SEC * 1/10; /* 100 ms */
@@ -4813,7 +4813,7 @@ SHOW_FUNCTION(cfq_fifo_expire_sync_show, cfqd->cfq_fifo_expire[1], 1);
 SHOW_FUNCTION(cfq_fifo_expire_async_show, cfqd->cfq_fifo_expire[0], 1);
 SHOW_FUNCTION(cfq_back_seek_max_show, cfqd->cfq_back_max, 0);
 SHOW_FUNCTION(cfq_back_seek_penalty_show, cfqd->cfq_back_penalty, 0);
-SHOW_FUNCTION(cfq_slice_idle_show, cfqd->cfq_slice_idle, 1);
+SHOW_FUNCTION(cfq_slice_idle_show, cfqd->cfq_slice_idle, 0);
 SHOW_FUNCTION(cfq_max_async_dispatch_show, cfqd->cfq_max_async_dispatch, 0);
 SHOW_FUNCTION(cfq_group_idle_show, cfqd->cfq_group_idle, 0);
 SHOW_FUNCTION(cfq_rt_idle_only_show, cfqd->cfq_rt_idle_only, 0);
@@ -4864,7 +4864,7 @@ STORE_FUNCTION(cfq_fifo_expire_async_store, &cfqd->cfq_fifo_expire[0], 1,
 STORE_FUNCTION(cfq_back_seek_max_store, &cfqd->cfq_back_max, 0, UINT_MAX, 0);
 STORE_FUNCTION(cfq_back_seek_penalty_store, &cfqd->cfq_back_penalty, 1,
 		UINT_MAX, 0);
-STORE_FUNCTION(cfq_slice_idle_store, &cfqd->cfq_slice_idle, 0, UINT_MAX, 1);
+STORE_FUNCTION(cfq_slice_idle_store, &cfqd->cfq_slice_idle, 0, UINT_MAX, 0);
 STORE_FUNCTION(cfq_max_async_dispatch_store, &cfqd->cfq_max_async_dispatch, 1, UINT_MAX, 0);
 STORE_FUNCTION(cfq_group_idle_store, &cfqd->cfq_group_idle, 0, UINT_MAX, 0);
 STORE_FUNCTION(cfq_rt_idle_only_store, &cfqd->cfq_rt_idle_only, 0, 1, 0);
